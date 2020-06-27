62ce88e562ba359a3e155173db345bcc0bb38fa0
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-06-27 04:29:42 +0300

    Updates

---
+++ precache-manifest.7f785296db02ea3bad5be944242dab11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cc760d9ab5dc390134a7a26a63bfe7",
+    "url": "/RN-hw-3-ImageFinder/index.html"
+  },
+  {
+    "revision": "134fd2695815d4e96ae2",
+    "url": "/RN-hw-3-ImageFinder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c45d3ffeff08369ecd33",
+    "url": "/RN-hw-3-ImageFinder/static/js/2.f8c8d618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RN-hw-3-ImageFinder/static/js/2.f8c8d618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134fd2695815d4e96ae2",
+    "url": "/RN-hw-3-ImageFinder/static/js/main.b8f4d225.chunk.js"
+  },
+  {
+    "revision": "c5e1beb3e7b55b9a3579",
+    "url": "/RN-hw-3-ImageFinder/static/js/runtime-main.00ac2740.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RN-hw-3-ImageFinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RN-hw-3-ImageFinder/precache-manifest.7f785296db02ea3bad5be944242dab11.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RN-hw-3-ImageFinder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
