7f1d57baa5cf327a3e341ea8017f5981cd4c4307
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-07-05 17:52:00 +0300

    root adding in fetch fix

--- imagefinder/src/services/apiImg.js
+++ imagefinder/src/services/apiImg.js
@@ -6,8 +6,7 @@ const apiKey = "16137003-a99878a83e3cf9a5973a72148";
 const fetchImg = ({ searchQuery, currentPage = 1, pageSize = 12 }) => {
   return axios
     .get(
-      root +
-        ` &q=${searchQuery}&per_page=${pageSize}&key=${apiKey}&page=${currentPage}`
+      `${root}&q=${searchQuery}&per_page=${pageSize}&key=${apiKey}&page=${currentPage}`
     )
     .then((response) => response.data.hits);
 };
